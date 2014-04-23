2451e25b27607dda2ca09765ad9fee399b76df38
Renato Carvalho <renatolz@gmail.com>
2014-04-23 15:17:31 -0300

    Update application.rb
    Signed-off-by: Renato Carvalho <renatolz@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ Bundler.require(*Rails.groups)
 
 module BrappsApi
   class Application < Rails::Application
+    config.i18n.default_locale = :'pt-BR'
+    config.time_zone = 'Brasilia'
+    Slim::Engine.set_default_options format: :html5
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
